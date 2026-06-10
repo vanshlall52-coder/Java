ee0f93568ceb1b079d9af95c8ab3001ce7d17446
Vansh52-coder <vanshlall52@gmail.com>
2026-06-10 22:38:14 +0530

    Add Initial File

---
+++ Operators/RelationalOperators.java
@@ -0,0 +1,19 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        // Relational operators --> == , != , < , > , <= , >=
+        int a = 10;
+        int b = 35;
+
+        boolean c = (a == b); // false
+         System.out.println(c);
+
+         boolean d = (a != b); // true
+         System.out.println(d);
+
+         boolean e = (a < b); // true
+         System.out.println(e);
+
+         boolean f = (a <= b);// true
+         System.out.println(f);
+    }
+}
