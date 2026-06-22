a4ab99180bd9038445f8f3cd087a692c3bd0303e
Vansh52-coder <vanshlall52@gmail.com>
2026-06-22 22:27:40 +0530

    Add new file

---
+++ Loops&Jump-Statements/PatternPrinting/DemoPattern.java
@@ -0,0 +1,22 @@
+package PatternPrinting;
+import java.util.Scanner;
+public class DemoPattern{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of i. ");
+        int I = sc.nextInt();
+
+        // System.out.print("Enter value of j. ");
+        // int J = sc.nextInt();
+
+        for(int i = 1; i <= I; i++){
+            for(int j = 1; j <= i; j++ ){
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
