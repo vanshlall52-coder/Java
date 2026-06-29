a169788be143805fa24a0010af9bb1a751d6113a
Vansh52-coder <vanshlall52@gmail.com>
2026-06-29 22:11:51 +0530

    Add new file

---
+++ Loops&Jump-Statements/PatternPrinting/NumberSquare2.java
@@ -0,0 +1,17 @@
+package PatternPrinting;
+import java.util.Scanner;
+public class NumberSquare2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of row: ");
+        int row = sc.nextInt();
+
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=row;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
