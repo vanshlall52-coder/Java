c0b3af091f4965d83295bb3a579b912335c22e85
Vansh52-coder <vanshlall52@gmail.com>
2026-07-12 10:36:46 +0530

    Add new file

--- Arrays/Array_2D.java
+++ Arrays/Array_2D.java
@@ -2,28 +2,28 @@ import java.util.Scanner;
 
 public class Array_2D {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        // Take number of rows and columns
         System.out.print("Enter number of rows: ");
         int rows = sc.nextInt();
 
         System.out.print("Enter number of columns: ");
         int cols = sc.nextInt();
 
-        // Create 2D array
         int[][] arr = new int[rows][cols];
 
-        // Input elements
-        System.out.print("Enter array elements:");
+        System.out.println("Enter elements:");
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
+                System.out.print("arr[" + i + "][" + j + "] = ");
                 arr[i][j] = sc.nextInt();
             }
         }
 
-        // Display array
-        System.out.println("Array elements are:");
+        System.out.println("Array:");
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 System.out.print(arr[i][j] + " ");
